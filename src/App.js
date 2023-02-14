c5e8212967e25540b7b46ff735b7f89f206cfe73
Benjamin Obafemi <benjaminoba234@gmail.com>
2023-02-14 07:24:12 +0300

    change background color

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { FormStateProvider } from './Context/FormContext';
 import FormPage from './pages/FormPage';
 function App() {
   return (
-    <div className='h-screen flex bg-lightgray font-ubuntu'>
+    <div className='h-screen flex bg-magnolia font-ubuntu'>
       <div className='w-full sm:mx-auto sm:my-auto'>
         <FormStateProvider>
           <FormPage />
